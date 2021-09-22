64d0be8f6bd1df1ae7efed208b17011c0ead7fe3
YnSen <65432445+YnSen@users.noreply.github.com>
2021-09-22 21:28:43 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -75,6 +75,3 @@ popd
 pushd package/tencentcloud-openwrt-plugin-ddns/tencentcloud_ddns/files/luci/controller
 sed -i 's/"admin", "tencentcloud"/"admin", "services", "tencentcloud"/g' tencentddns.lua
 popd
-
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
