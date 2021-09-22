5cc9a6a0f6ef8dca7c85b3bdb9ce51e7820a947a
YnSen <65432445+YnSen@users.noreply.github.com>
2021-09-22 21:27:03 +0800

    Update opclone_source.sh

--- opclone_source.sh
+++ opclone_source.sh
@@ -1,2 +1,2 @@
 git clone -b openwrt-21.02 https://github.com/openwrt/openwrt.git
-pushd "openwrt"; ./scripts/feeds update -a; ./scripts/feeds install -a; popd
+pushd "openwrt"; git checkout v21.02.0 && ./scripts/feeds update -a; ./scripts/feeds install -a; popd
