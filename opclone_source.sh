c1b554596c0899067af7e5f6e0f6d0de70b9f577
YnSen <65432445+YnSen@users.noreply.github.com>
2021-09-28 10:07:22 +0800

    Update opclone_source.sh

--- opclone_source.sh
+++ opclone_source.sh
@@ -3,7 +3,7 @@ pushd "openwrt"
 git checkout v21.02.0
 sed -i 's/src-git luci/#src-git luci/g' feeds.conf.default
 sed -i 's/src-git packages/#src-git packages/g' feeds.conf.default
-sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^31a0e8a'feeds.conf.default
-sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^7ce5631 feeds.conf.default
+sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^31a0e8a' feeds.conf.default
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^7ce5631' feeds.conf.default
 ./scripts/feeds update -a; ./scripts/feeds install -a
 popd
