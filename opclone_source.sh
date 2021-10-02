bd85eb40603b2fd266461e0f6b99515cc70bb117
YnSen <65432445+YnSen@users.noreply.github.com>
2021-10-02 10:19:52 +0800

    Delete opclone_source.sh

--- opclone_source.sh
+++
@@ -1,9 +0,0 @@
-git clone -b openwrt-21.02 https://github.com/openwrt/openwrt.git
-pushd "openwrt"
-git checkout v21.02.0
-sed -i 's/src-git luci/#src-git luci/g' feeds.conf.default
-sed -i 's/src-git packages/#src-git packages/g' feeds.conf.default
-sed -i '$a src-git luci https://github.com/immortalwrt/luci.git^31a0e8a' feeds.conf.default
-sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^7ce5631' feeds.conf.default
-./scripts/feeds update -a; ./scripts/feeds install -a
-popd
